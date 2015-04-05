9ded3ab3c61216b0ec7944e954880a32135ebbf0
vassi95 <vassi__95@abv.bg>
2015-04-05 18:20:26 +0300

    commit

---
+++ Week3/BankAccount/bank.py
@@ -0,0 +1,79 @@
+
+
+class BankAccount:
+
+    def __init__(self, name, amount, currency):
+        self.name = name
+        self.amount = amount
+        self.currency = currency
+        self.__history = ["Account was created"]
+
+    def __str__(self):
+        message = "Bank account for {} with balance of {}{}"
+        return message.format(self.name, self.amount, self.currency)
+
+    def __int__(self):
+        self.__history.append('__int__ check -> {}{}'.format(
+            self.amount, self.currency))
+        return self.balance()
+
+    def deposit(self, money):
+        if money < 0:
+            raise ValueError
+        else:
+            self.amount += money
+            self.__history.append('Deposited {}{}'.format(money, self.currency))
+
+    def balance(self):
+        self.__history.append("Balance -> {}{}".format(self.amount, self.currency))
+        return self.amount
+
+    def withdraw(self, money):
+        if self.amount < money:
+            self.__history.append("Withdraw for {}{} failed".format(
+                self.amount, self.currency))
+            return False
+            raise ValueError
+        else:
+            self.amount -= money
+            self.__history.append("{}{} was withdrawn".format(
+                money, self.currency))
+            return True
+
+    def transfer_to(self, account, amount):
+        if self.currency != account.currency:
+            raise ValueError
+        if self.balance < amount:
+            return False
+        else:
+            self.withdraw(amount)
+            account.deposit(amount)
+            self.__history.append("Transfer to {} for {}{}".format(
+                account.name, amount, self.currency))
+        return True
+
+    def history(self):
+        return self.__history
+
+
+if __name__ == '__main__':
+    account = BankAccount("Rado", 0, "$")
+    print(account)
+    account.deposit(1000)
+    print(account.balance())
+    print(str(account))
+    print(int(account))
+    print(account.history())
+    print(account.withdraw(500))
+    print(account.balance())
+    print(account.history())
+    print(account.withdraw(1000))
+    print(account.balance())
+    print(account.history())
+    rado = BankAccount("Rado", 1000, "$")
+    ivo = BankAccount("Ivo", 0, "$")
+    print(rado.transfer_to(ivo, 500))
+    print(rado.balance())
+    print(ivo.balance())
+    print(rado.history())
+    print(ivo.history())
