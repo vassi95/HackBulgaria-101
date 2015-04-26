d2efedc9e7b85f9b2d0216ee6e96795823e3a766
vassi95 <vassi__95@abv.bg>
2015-04-26 13:32:37 +0300

    commit

--- Week3/BankAccount/bank_test.py
+++ Week3/BankAccount/bank_test.py
@@ -8,27 +8,34 @@ class Test_BankAccount(unittest.TestCase):
         self.rado = BankAccount("Rado", 1000, "$")
         self.ivo = BankAccount("Ivo", 0, "$")
 
-    def test_create_account(self):
-        self.assertIsInstance(self.rado, BankAccount)
-        self.assertIsInstance(self.ivo, BankAccount)
+    def test_init(self):
+        self.assertEqual(self.rado.name, 'Rado')
+        self.assertEqual(self.rado.amount, 1000)
+        self.assertEqual(self.rado.currency, '$')
+        self.assertEqual(self.ivo.name, 'Ivo')
+        self.assertEqual(self.ivo.amount, 0)
+        self.assertEqual(self.ivo.currency, '$')
+
+    def test_str(self):
+        self.rado = BankAccount("Rado", 1000, "$")
+        self.assertEqual(str(self.rado),
+                         "Bank account for {} with balance of {}{}".format(
+                self.rado.name, self.rado.amount, self.rado.currency))
+        self.ivo = BankAccount("Ivo", 0, "$")
+        self.assertEqual(str(self.ivo),
+                         "Bank account for {} with balance of {}{}".format(
+                self.ivo.name, self.ivo.amount, self.ivo.currency))
 
     def test_deposit(self):
         with self.assertRaises(ValueError):
             self.ivo.deposit(-10)
 
-    def test_return_balance(self):
+    def test_balance(self):
         self.assertEqual(self.rado.amount, 1000)
 
-    def test_withdraw_from_non_empty_account(self):
-        self.rado.deposit(100)
-        result = self.rado.withdraw(50)
-        self.assertTrue(result)
-        self.assertEqual(self.rado.balance(), 1050)
-
-    def test_withdraw_from_empty_account(self):
-        result = self.rado.withdraw(10000)
-        self.assertIsNotNone(result)
-        self.assertFalse(result)
+    def test_withdraw(self):
+        with self.assertRaises(ValueError):
+            self.rado.withdraw(2000)
 
     def test_string(self):
         self.assertEqual(str(self.ivo),
@@ -36,9 +43,10 @@ class Test_BankAccount(unittest.TestCase):
             self.ivo.name, self.ivo.amount, self.ivo.currency))
 
     def test_int(self):
-        self.assertTrue(int(self.ivo) == 0)
+        self.assertTrue(int(self.ivo.amount) == 0)
+        self.assertTrue(int(self.rado.amount) == 1000)
 
-    def test_transfer(self):
+    def test_transfer_to(self):
         self.assertTrue(self.rado.transfer_to(self.ivo, 10))
 
     def test_history(self):
