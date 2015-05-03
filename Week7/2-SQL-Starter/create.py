92243b939a4f84059f6e88460d46332e7c287179
vassi95 <vassi__95@abv.bg>
2015-05-03 10:45:03 +0300

    update

--- Week7/2-SQL-Starter/create.py
+++ Week7/2-SQL-Starter/create.py
@@ -3,6 +3,9 @@ import sqlite3
 
 class Table:
 
+    def __init__(self):
+        pass
+
     def create(self):
         db = sqlite3.connect('Table.db')
         cursor = db.cursor()
@@ -10,12 +13,21 @@ class Table:
             CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT,
                                monthly_salary INTEGER, yearly_bonus INTEGER, possition TEXT)
         ''')
-        cursor.execute("INSERT INTO users(name, monthly_salary, yearly_bonus, possition) VALUES('Ivan Ivanov', 5000, 10000, 'Software Developer')")
-        cursor.execute("INSERT INTO users(name, monthly_salary, yearly_bonus, possition) VALUES('Rado Rado', 500, 0, 'Technical Support Intern')")
-        cursor.execute("INSERT INTO users(name, monthly_salary, yearly_bonus, possition) VALUES('Ivo Ivo', 10000, 100000, 'CEO')")
-        cursor.execute("INSERT INTO users(name, monthly_salary, yearly_bonus, possition) VALUES('Petar Petrov', 3000, 1000, 'Marketing Manager')")
-        cursor.execute("INSERT INTO users(name, monthly_salary, yearly_bonus, possition) VALUES('Maria Georgieva', 8000, 10000, 'COO')")
         db.commit()
+
+    def fill_in_table(self):
+        conn = sqlite3.connect('Table.db')
+        cursor = conn.cursor()
+        employees = int(input("Enter the numbe of employees in the company: "))
+        i = 0
+        while i < employees:
+            name = input("enter name:  ")
+            ms = int(input("enter monthly_salary:  "))
+            yb = int(input("enter yearly_bonus:  "))
+            pos = input("enter possition:  ")
+            cursor.execute("INSERT INTO users(name, monthly_salary, yearly_bonus, possition) VALUES(?, ?, ?, ?)", (name, ms, yb, pos))
+            i += 1
+            conn.commit()
 
 if __name__ == "__main__":
     b = Table()
