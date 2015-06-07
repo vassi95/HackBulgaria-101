7aa3da7f414d4166c6253e04b65438342c90dc7f
vassi95 <vassi__95@abv.bg>
2015-06-07 18:31:25 +0300

    ..

--- Week10/django_cinema/website/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Movie, Projection, Rating
-
-admin.site.register(Movie)
-admin.site.register(Projection)
-admin.site.register(Rating)
--- Week10/django_cinema/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^show_movie/$', views.show_movie),
-    url(r'^rate_movie/$', views.rate_movie)
-]
